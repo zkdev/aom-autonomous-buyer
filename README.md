dca6b022b87edb1fc78ee12ed4e90bd379a28e6c
zkdev <dev@zeekay.dev>
2020-07-03 18:06:30 +0200

    Add hotdeals search for itemname

--- README.md
+++ README.md
@@ -3,9 +3,26 @@ This little script allows you to automatically buy items from the Age of Menor i
 <br>
 **Disclaimer**
 <br>
-Use this tool on your own risk. Little to no coding knowhow is necessary to understand that this script does not log your credentials. The only point of failure is providing the wrong `itemid`. Thus the tool would buy an unintended item but it is obviously your fault.
+Use this tool on your own risk. Little to no coding knowhow is necessary to understand that this script does not log your credentials.
   
-## Usage  
+## How it works
+This tool accepts two ways to specify the desired item.<br>
+You can either provide the `--itemid` directly or specifying an `--itemname`.<br>
+In case you provide the `--itemid` the tool perfoms `POST shop.ageofmenor.cc/mall/purchases/itemid`.<br>
+If you just provide an `--itemname` a list of all Hotdeals is collected and searched for the given `--itemname`.<br>
+The search itself extracts the itemshop entry titel and checks whether it contains the provided `--itemname`.<br>
+If it finds a match the id is extracted and `POST shop.ageofmenor.cc/mall/purchases/itemid` performed.<br>
+
+## Installation  
 1. Navigate into `.../aom-buyer`
 2. Install dependencies using `pip3 install -r requirements.txt` 
-3. Execute python script `python3 -m src.main --username "USERNAME" --password "PASSWORD" --itemid ITEMID --desired-time "13:54:01"`
+
+## Usage
+### Provide itemid directly
+`python3 -m src.main --username "USERNAME" --password "PASSWORD" --itemid ITEMID --desired-time "13:54:01"`
+
+### Search Hotdeals for itemname
+`python3 -m src.main --username "USERNAME" --password "PASSWORD" --itemname "Shining" --desired-time "13:54:01"`
+
+## Potential bugs
+- If you are on a Unix system provide arguments containing a special character using single quotes.
--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import sys
 
 SIGNIN_ENDPOINT = "https://shop.ageofmenor.cc/auth/signin"
 PURCHASE_ENDPOINT = "https://shop.ageofmenor.cc/mall/purchases/{}"
+HOTDEAL_ENDPOINT = "https://shop.ageofmenor.cc/mall/1/hot"
 csrf_data = {
     "csrf_value": None,
     "csrf_name": None
@@ -34,22 +35,46 @@ def purchase_item(session, itemid):
     logger.info('Attempt to buy itemid {}'.format(str(itemid)))
 
 
+def get_correct_hotdeal_id(hotdeals, itemname):
+    for titel in hotdeals:
+        if titel.lower().__contains__(str(itemname).lower()):
+            logger.info("Hotdeal id found: {} - {}".format(titel, hotdeals[titel]))
+            return hotdeals[titel]
+    logger.error('No hotdeal for {} found.'.format(str(itemname)))
+    sys.exit(1)
+
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--username', action='store', dest='username')
-    parser.add_argument('--password', action='store', dest='password')
-    parser.add_argument('--itemid', action='store', dest='itemid')
-    parser.add_argument('--desired-time', action='store', dest='desired_time')
+    parser.add_argument('--username', action='store', dest='username', required=True)
+    parser.add_argument('--password', action='store', dest='password', required=True)
+    parser.add_argument('--itemid', action='store', dest='itemid', required=False)
+    parser.add_argument('--desired-time', action='store', dest='desired_time', required=True    )
+    parser.add_argument('--itemname', action='store', dest='item_name', required=False)
     return parser.parse_args()
 
 
 def is_desired_time(desired_time):
     while True:
-        if str(datetime.datetime.now().strftime('%H:%M:%S.%f')[:-7]) == str(desired_time):
+        if str(datetime.datetime.now().strftime('%H:%M:%S.%f')[:-7]) == str(desired_time) or desired_time is None:
             return True
         else:
             logger.info('Waiting for desired time ... ({} != {})'.format(
                 datetime.datetime.now().strftime('%H:%M:%S.%f')[:-7], desired_time))
+
+
+def get_hotdeals(session):
+    deals = {}
+    r = session.get(HOTDEAL_ENDPOINT)
+    soup = BeautifulSoup(r.content, features="html.parser")
+    for li in soup.select_one('div#main-item-box-container').contents[1].findAll('li'):
+        try:
+            id = str(li.contents[1].attrs["action"]).replace('/mall/purchases/', '')
+            titel = str(li.contents[1].contents[1].contents[1].contents[0])
+            deals[titel] = id
+        except:
+            pass
+    return deals
 
 
 if __name__ == "__main__":
@@ -57,5 +82,15 @@ if __name__ == "__main__":
     logger = logging.Logger(__name__)
     s = get_session(username=args.username, password=args.password)
     if is_desired_time(args.desired_time):
-        purchase_item(session=s, itemid=args.itemid)
+        if args.itemid is None:
+            if args.item_name is not None:
+                d = get_hotdeals(s)
+                id = get_correct_hotdeal_id(hotdeals=d, itemname=args.item_name)
+                purchase_item(session=s, itemid=id)
+            else:
+                logger.error("Whether itemid nor itemname was provided.")
+                sys.exit(1)
+        else:
+            purchase_item(session=s, itemid=args.itemid)
+            pass
     sys.exit(0)
